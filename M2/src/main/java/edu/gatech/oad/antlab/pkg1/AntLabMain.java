d4642d833cce60fa78fe4870e9e9542da6dca817
jazl32 <31395637+jazl32@users.noreply.github.com>
2017-08-29 22:12:09 -0400

    p3 edit antlabmain
    send "jasmine" to reverse method

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -57,8 +57,8 @@ import edu.gatech.oad.antlab.pkg2.*;
           + ant22.getMessage() + ant31.getMessage();
 		  //Person1 replace P1 with your name
 		  //and gburdell1 with your gt id
-		  Person1 p1 = new Person1("Shannon");
-		  toPrint += p1.toString("shwu6");
+		  Person1 p1 = new Person1("P1");
+		  toPrint += p1.toString("gburdell1");
 		  //Person2 replace P2 with your name
 		  //and gburdell with your gt id
 		  Person2 p2 = new Person2("P2");
@@ -66,15 +66,15 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
 		  Person3 p3 = new Person3("P3");
-		  toPrint += p3.toString("gburdell3");
+		  toPrint += p3.toString("jasmine");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
                   Person4 p4 = new Person4("P4");
                   toPrint += p4.toString("gburdell4");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("Jason");
-                  toPrint += p5.toString("jchan72");
+                  Person5 p5 = new Person5("P5");
+                  toPrint += p5.toString("gburdell5");
 		  
         System.out.println(toPrint);
         
