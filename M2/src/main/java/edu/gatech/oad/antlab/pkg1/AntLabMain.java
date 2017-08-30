3ee63d94671a9b08149253e92bfe21894a15895d
jazl32 <31395637+jazl32@users.noreply.github.com>
2017-08-29 22:37:57 -0400

    Update AntLabMain.java

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -65,8 +65,8 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p2.toString("cliu432");
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
-		  Person3 p3 = new Person3("P3");
-		  toPrint += p3.toString("jasmine");
+		  Person3 p3 = new Person3("Jasmine");
+		  toPrint += p3.toString("jliu614");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
                   Person4 p4 = new Person4("P4");
