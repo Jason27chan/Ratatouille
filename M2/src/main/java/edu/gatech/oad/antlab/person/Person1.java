dd73c8449c36366226f682d5c593e6977128013e
guess-hwu <shany198@gmail.com>
2017-09-03 11:57:56 -0400

    Person 1 calc method edit (commit test through Eclipse IDE)

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Shannon Hwu
  *  @version 1.1
  */
 public class Person1 {
@@ -30,8 +30,10 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+	  String last = input.substring(0,2);
+	  String first = input.substring(2);
+	  String final =  first + last
+	  return final;
 	}
 	
 	/**
