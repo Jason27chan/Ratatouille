d0158c1aeb4b55756cd5b1fdb507685c9e1a3038
j <jasmineliu@gatech.edu>
2017-09-12 18:45:16 -0400

    added registration and login activities

---
+++ Ratatouille_Project/app/src/main/java/com/example/jl/ratatouille/RegistrationActivity.java
@@ -0,0 +1,19 @@
+package com.example.jl.ratatouille;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jav on 9/12/2017.
+ */
+
+public class RegistrationActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+
+    }
+}
