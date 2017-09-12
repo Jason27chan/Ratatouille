a3d46da22f9de2448b8a37b92bb41c1c242b275d
cathy-liu23 <liu.cathy423@gmail.com>
2017-09-12 19:44:13 -0400

    activity.main edits
    Finished buttons
    Formatted buttons and text to center of page
    Created a string

--- Ratatouille_Project/app/src/main/java/com/example/jl/ratatouille/MainActivity.java
+++ Ratatouille_Project/app/src/main/java/com/example/jl/ratatouille/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.jl.ratatouille;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
