842a958d2119a4616f3d97703537e05073e442fe
j <jasmineliu@gatech.edu>
2017-09-12 21:01:02 -0400

    Registration

--- Ratatouille_Project/app/src/main/java/com/example/jl/ratatouille/RegistrationActivity.java
+++ Ratatouille_Project/app/src/main/java/com/example/jl/ratatouille/RegistrationActivity.java
@@ -5,47 +5,55 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.jl.ratatouille.db.DataBaseAdapter;
 
 /**
  * Created by jav on 9/12/2017.
  */
 
-public class RegistrationActivity extends AppCompatActivity implements View.OnClickListener{
+public class RegistrationActivity extends AppCompatActivity {
 
     private static final int REGISTER_USER_REQUEST_CODE = 1;
+
+    private String newUsername;
+    private String newPassword;
+
+    private EditText usernameEntry;
+    private EditText passwordEntry;
+
+    private Button registrationButton;
+
+    private DataBaseAdapter dataBaseAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        Button registrationButton = (Button) findViewById(R.id.registration_button_register);
-        registrationButton.setOnClickListener(this);
+        //get database adapter
+        dataBaseAdapter = new DataBaseAdapter(this);
+
+
+        usernameEntry = (EditText) findViewById(R.id.registration_edit_username);
+        passwordEntry = (EditText) findViewById(R.id.registration_edit_password);
+
+        //get registration button reference
+        registrationButton = (Button) findViewById(R.id.registration_button_register);
+
+        registrationButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                //get entered username and password
+                newUsername = usernameEntry.getText().toString();
+                newPassword = passwordEntry.getText().toString();
+
+                //save the data in database
+                dataBaseAdapter.insertEntry(newUsername, newPassword);
+
+            }
+        });
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.registration_button_register:
-                registerNewUser();
-                break;
-
-            case R.id.registration_button_cancel:
-                finish();
-        }
-
-    }
-
-    private void registerNewUser() {
-
-        Intent registerIntent = new Intent();
-
-        startActivityForResult(registerIntent, REGISTER_USER_REQUEST_CODE);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-    }
 }
---
+++ Ratatouille_Project/app/src/main/java/com/example/jl/ratatouille/db/DataBaseHelper.java
@@ -0,0 +1,8 @@
+package com.example.jl.ratatouille.db;
+
+/**
+ * Created by jav on 9/12/2017.
+ */
+
+public class DataBaseHelper {
+}
