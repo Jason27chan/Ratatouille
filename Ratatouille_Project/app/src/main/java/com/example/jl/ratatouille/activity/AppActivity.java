996a1624ad2784a97cc44e8453b105e253c73f7d
j <jasmineliu@gatech.edu>
2017-09-25 02:20:21 -0400

    update

--- Ratatouille_Project/app/src/main/java/com/example/jl/ratatouille/activity/AppActivity.java
+++ Ratatouille_Project/app/src/main/java/com/example/jl/ratatouille/activity/AppActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
 import com.example.jl.ratatouille.R;
 
 public class AppActivity extends AppCompatActivity {
