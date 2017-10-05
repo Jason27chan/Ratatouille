cd7aa11cd451d7fdf433d1bc2a96ef4fcf481e62
hallowpeno <chang.emily98@gmail.com>
2017-10-05 19:33:45 -0400

    made rat class

---
+++ Ratatouille_Project/app/src/main/java/com/example/jl/ratatouille/Rat.java
@@ -0,0 +1,19 @@
+package com.example.jl.ratatouille;
+
+/**
+ * Created by Emily Chang on 10/5/2017.
+ */
+
+public class Rat {
+    private int key;
+    private int date;
+    private String locType;
+    private int incidentZip;
+    private String incidentAddress;
+    private String city;
+    private String borough;
+    private int latitude;
+    private int longitude;
+
+    
+}
