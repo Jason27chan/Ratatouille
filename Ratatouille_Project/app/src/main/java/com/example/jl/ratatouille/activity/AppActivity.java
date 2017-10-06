0d50d29438888bbedbada783fae1774b285a154d
Jason Chan <jchan72@gatech.edu>
2017-10-06 19:29:53 -0400

    attempt for table

--- Ratatouille_Project/app/src/main/java/com/example/jl/ratatouille/activity/AppActivity.java
+++ Ratatouille_Project/app/src/main/java/com/example/jl/ratatouille/activity/AppActivity.java
@@ -4,9 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.example.jl.ratatouille.R;
 import com.example.jl.ratatouille.db.CSVFile;
@@ -43,25 +44,35 @@ public class AppActivity extends AppCompatActivity {
         CSVFile csvFile = new CSVFile(inputStream);
         List scoreList = csvFile.read();
 
+        // for all String arrays in scoreList
+        //  create new TableRow
+        //  for all elements in intArray
+        //      Add column to tableRow
+        //  add tableRow to tableLayout
 
 
-        ArrayAdapter<String> ratAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
-        for (int i = 0; i < 3; i++) {
-            ratAdapter.addAll((String[]) scoreList.get(i));
-        }
+//        ArrayAdapter<String> ratAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+//        for (int i = 0; i < 3; i++) {
+//            ratAdapter.addAll((String[]) scoreList.get(i));
+//        }
 
 
         int[] intArray = {0, 1, 7, 8, 9, 16, 23, 30, 29};
-        
-        for (i : intArray) {
-
-        }
-
         TableLayout table = (TableLayout) findViewById(R.id.ratTable);
-        int count = ratAdapter.getCount();
-        for (int i = 0; i < count; i++) {
-            table.addView(ratAdapter.getView(i, null, table));
+
+        for (int i = 0; i < 50; i++) {
+            TableRow row = new TableRow(table.getContext());
+            for (int j = 0; j < intArray.length; j++) {
+                TextView text = new TextView(table.getContext());
+                text.setText(((String[]) scoreList.get(i))[intArray[j]]);
+            }
+            table.addView(row);
         }
+
+//        int count = ratAdapter.getCount();
+//        for (int i = 0; i < count; i++) {
+//            table.addView(ratAdapter.getView(i, null, table));
+//        }
 
 //        ListView ratData = (ListView) findViewById(R.id.ratList);
 //        ratData.setAdapter(ratAdapter);
