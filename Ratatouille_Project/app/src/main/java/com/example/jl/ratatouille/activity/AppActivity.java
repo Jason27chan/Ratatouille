590eb97fc14b63e413bba019d00aaebd1d8b7b7c
Jason Chan <jchan72@gatech.edu>
2017-10-06 19:34:24 -0400

    second attempt table works

--- Ratatouille_Project/app/src/main/java/com/example/jl/ratatouille/activity/AppActivity.java
+++ Ratatouille_Project/app/src/main/java/com/example/jl/ratatouille/activity/AppActivity.java
@@ -65,6 +65,7 @@ public class AppActivity extends AppCompatActivity {
             for (int j = 0; j < intArray.length; j++) {
                 TextView text = new TextView(table.getContext());
                 text.setText(((String[]) scoreList.get(i))[intArray[j]]);
+                row.addView(text);
             }
             table.addView(row);
         }
