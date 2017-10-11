b659dd1102eb92b2979b1a13e5e87c0d19112e08
cathy-liu23 <liu.cathy423@gmail.com>
2017-10-10 22:06:56 -0400

    Updated javadocs for Rat Class

--- Ratatouille_Project/app/src/main/java/com/example/jl/ratatouille/model/Rat.java
+++ Ratatouille_Project/app/src/main/java/com/example/jl/ratatouille/model/Rat.java
@@ -20,10 +20,27 @@ public class Rat implements Parcelable {
     private String latitude;
     private String longitude;
 
+    /**
+     * Default constructor for Rat with no data passed in
+     */
     public Rat() {
         this("unknown", "unknown", "unknown", "unknown", "unknown", "unknown", "unknown", "unknown", "unknown");
     }
 
+    /**
+     * a constructor for the rat
+     * with all parameters passed in
+     *
+     * @param id the id for the rat
+     * @param date the date that the rat was located
+     * @param locType the location type of the rat
+     * @param zip the zip code that the rat is located at
+     * @param address the address of the rat
+     * @param city the city of the rat
+     * @param borough the borough of the rat
+     * @param latitude the latitude at which the rat was found
+     * @param longitude the longitude at which the rat was found
+     */
     public Rat(String id, String date, String locType, String zip, String address, String city, String borough, String latitude, String longitude) {
         this.id = id;
         this.date = date;
@@ -36,18 +53,34 @@ public class Rat implements Parcelable {
         this.longitude = longitude;
     }
 
+    /**
+     * getter for the rat id
+     * @return the id of the rat
+     */
     public String getRatId() {
         return id;
     }
 
+    /**
+     * setter for the rat id
+     * @param id the new id for the rat
+     */
     public void setRatId(String id) {
         this.id = id;
     }
 
+    /**
+     * gets the date that the rat was found
+     * @return the date that the rat was found
+     */
     public String getDate() {
         return date;
     }
 
+    /**
+     * sets the date at which the rat was found
+     * @param date the date at which the rat was spotted
+     */
     public void setDate(String date) {
         if (date.equals("") || date == null) {
             return;
@@ -55,26 +88,50 @@ public class Rat implements Parcelable {
         this.date = date;
     }
 
+    /**
+     * gets the location type of the rat
+     * @return the location type of the rat
+     */
     public String getLocType() {
         return locType;
     }
 
+    /**
+     * sets the location type for the rat
+     * @param locType the new location type for the rat
+     */
     public void setLocType(String locType) {
         this.locType = locType;
     }
 
+    /**
+     * gets the zip code of the rat
+     * @return the zip code of the rat
+     */
     public String getZip() {
         return zip;
     }
 
+    /**
+     * sets the new zip code for the rat
+     * @param zip the new zip code for the rat
+     */
     public void setZip(String zip) {
         this.zip = zip;
     }
 
+    /**
+     * gets the address of the rat
+     * @return the address of the rat
+     */
     public String getAddress() {
         return address;
     }
 
+    /**
+     * sets the new address for the rat
+     * @param address the new address for the rat
+     */
     public void setAddress(String address) {
         if (address.equals("") || address == null) {
             return;
@@ -82,10 +139,18 @@ public class Rat implements Parcelable {
         this.address = address;
     }
 
+    /**
+     * gets the city that the rat is located in
+     * @return the city that the rat is located in
+     */
     public String getCity() {
         return city;
     }
 
+    /**
+     * sets the new city that the rat is located in
+     * @param city the new city that the rat is located in
+     */
     public void setCity(String city) {
         if (city.equals("") || city == null) {
             return;
@@ -93,26 +158,50 @@ public class Rat implements Parcelable {
         this.city = city;
     }
 
+    /**
+     * gets the borough that the rat is in
+     * @return the borough the rat is in
+     */
     public String getBorough() {
         return borough;
     }
 
+    /**
+     * sets the borough that the rat is in
+     * @param borough the new borough that the rat is in
+     */
     public void setBorough(String borough) {
         this.borough = borough;
     }
 
+    /**
+     * gets the latitude at which the rat is at
+     * @return the latitude at which the rat is at
+     */
     public String getLatitude() {
         return latitude;
     }
 
+    /**
+     * sets the new latitude at which the rat is at
+     * @param latitude the new latitude of the rat
+     */
     public void setLatitude(String latitude) {
         this.latitude = latitude;
     }
 
+    /**
+     * gets the longitude at which the rat is at
+     * @return the longitude at which the rat is at
+     */
     public String getLongitude() {
         return longitude;
     }
 
+    /**
+     * sets the new longitude at which the rat is at
+     * @param longitude the new longitude at which the rat is at
+     */
     public void setLongitude(String longitude) {
         this.longitude = longitude;
     }
@@ -136,15 +225,29 @@ public class Rat implements Parcelable {
     }
 
     public static final Parcelable.Creator<Rat> CREATOR = new Parcelable.Creator<Rat>() {
+        /**
+         * creating a new rat from parcel
+         * @param in the parcel in which the object's data is stored
+         * @return new instance of the parcelable class
+         */
         public Rat createFromParcel(Parcel in) {
             return new Rat(in);
         }
 
+        /**
+         * creates a new array that can hold Rat objects that is a specific size
+         * @param size size of the array
+         * @return the new Rat array of the specified size
+         */
         public Rat[] newArray(int size) {
             return new Rat[size];
         }
     };
 
+    /**
+     * creates the new instance of the parcelable class
+     * @param in the parcel in which the data is to be stored
+     */
     private Rat(Parcel in) {
         id = in.readString();
         date = in.readString();
