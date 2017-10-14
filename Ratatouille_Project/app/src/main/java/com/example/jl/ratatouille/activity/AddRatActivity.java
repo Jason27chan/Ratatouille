a3919e5583b3041b35856ba6f84529c73b932d48
Jason Chan <jchan72@gatech.edu>
2017-10-13 21:04:48 -0400

    create activity

---
+++ Ratatouille_Project/app/src/main/java/com/example/jl/ratatouille/activity/AddRatActivity.java
@@ -0,0 +1,17 @@
+package com.example.jl.ratatouille.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.jl.ratatouille.R;
+
+public class AddRatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_rat);
+
+    }
+
+}
