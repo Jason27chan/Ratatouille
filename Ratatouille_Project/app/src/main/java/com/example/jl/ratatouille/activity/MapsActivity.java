1125243d06f032bf2f6901af9bb8e9be729c340f
j <jasmineliu@gatech.edu>
2017-10-18 22:07:04 -0400

    implemented asynctask, added progress spinner, updated build file

---
+++ Ratatouille_Project/app/src/main/java/com/example/jl/ratatouille/activity/MapsActivity.java
@@ -0,0 +1,8 @@
+package com.example.jl.ratatouille.activity;
+
+/**
+ * Created by jav on 10/18/2017.
+ */
+
+public class MapsActivity {
+}
--- Ratatouille_Project/app/src/main/java/com/example/jl/ratatouille/activity/RatListActivity.java
+++ Ratatouille_Project/app/src/main/java/com/example/jl/ratatouille/activity/RatListActivity.java
@@ -1,13 +1,17 @@
 package com.example.jl.ratatouille.activity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 
 import com.example.jl.ratatouille.R;
 import com.example.jl.ratatouille.adapter.RatListAdapter;
@@ -19,6 +23,8 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import static android.R.attr.start;
+
 
 /**
  * Displays the rat data in a RecyclerView
@@ -29,43 +35,19 @@ public class RatListActivity extends AppCompatActivity {
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private List<Rat> ratList;
+    private int startIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rat_list);
-        mRecyclerView = (RecyclerView) findViewById(R.id.rat_recycler_view);
 
-        mRecyclerView.setHasFixedSize(true);
-
-        //set layout
-        mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-
-        //add divider lines
-        DividerItemDecoration mDividerItemDecoration = new DividerItemDecoration(mRecyclerView.getContext(), LinearLayoutManager.VERTICAL);
-        mRecyclerView.addItemDecoration(mDividerItemDecoration);
-
-        //set adapter
-        ratList = new ArrayList<>();
-        mAdapter = new RatListAdapter(ratList, this);
-        mRecyclerView.setAdapter(mAdapter);
-
-        loadRatData();
-
-        //set endless scroll
-        mRecyclerView.addOnScrollListener(new EndlessOnScrollListener() {
-            @Override
-            public void onLoadMore() {
-                loadRatData();
-            }
-        });
-
-        //todo: add button which leads to filter options activity
-
+        setupRecyclerView();
+        loadRatData(startIndex, startIndex + 25);
+        setupEndlessScroll();
 
         //add rat button
-        final Button addRatBtn = (Button) findViewById(R.id.btn_addRat);
+        final Button addRatBtn = findViewById(R.id.btn_addRat);
         addRatBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent myIntent = new Intent(v.getContext(), AddRatActivity.class);
@@ -75,7 +57,7 @@ public class RatListActivity extends AppCompatActivity {
 
 
         //logout button
-        final Button logoutBtn = (Button) findViewById(R.id.btn_logout);
+        final Button logoutBtn = findViewById(R.id.btn_logout);
         logoutBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent myIntent = new Intent(v.getContext(), MainActivity.class);
@@ -84,38 +66,79 @@ public class RatListActivity extends AppCompatActivity {
         });
     }
 
-    //todo: add filtering options and endless scroll
+    private void setupEndlessScroll() {
+        mRecyclerView.addOnScrollListener(new EndlessOnScrollListener() {
+            @Override
+            public void onLoadMore() {
+                startIndex += 25;
+                loadRatData(startIndex, startIndex + 25);
+            }
+        });
+    }
 
-    /**
-     * Fills the ratList with the data pulled from the CSV file
-     */
-    private void loadRatData() {
-        InputStream inputStream = getResources().openRawResource(R.raw.rat_sightings_trimmed);
-        CSVFile csvFile = new CSVFile(inputStream);
-        List rowList = csvFile.read();
-        for (int i = 0; i < 50; i++) {
-            Rat rat = new Rat();
-            String[] row = (String[]) rowList.get(i);
-            String id = row[0];
-            String date = row[1];
-            String locType = row[2];
-            String zip = row[3];
-            String address = row[4];
-            String city = row[5];
-            String borough = row[6];
-            String latitude = row[7];
-            String longitude = row[8];
-            rat.setRatId(id);
-            rat.setDate(date);
-            rat.setLocType(locType);
-            rat.setZip(zip);
-            rat.setAddress(address);
-            rat.setCity(city);
-            rat.setBorough(borough);
-            rat.setLatitude(latitude);
-            rat.setLongitude(longitude);
-            ratList.add(rat);
+    private void loadRatData(int start, int end) {
+        new LoadDataTask().execute(R.raw.rat_sightings_trimmed, start, end);
+    }
+
+    private class LoadDataTask extends AsyncTask<Integer, Void, String> {
+        private ProgressBar spinner;
+
+        @Override
+        protected void onPreExecute() {
+            spinner = findViewById(R.id.progressBar_ratList);
+            spinner.setVisibility(View.VISIBLE);
         }
-        mAdapter.notifyDataSetChanged();
+        @Override
+        protected String doInBackground(Integer... fileName) {
+            InputStream inStream = getResources().openRawResource(fileName[0]);
+            CSVFile csvFile = new CSVFile(inStream);
+            List rowList = csvFile.read();
+            for (int i = fileName[1]; i < fileName[2]; i++) {
+                Rat rat = new Rat();
+                String[] row = (String[]) rowList.get(i);
+                String id = row[0];
+                String date = row[1];
+                String locType = row[2];
+                String zip = row[3];
+                String address = row[4];
+                String city = row[5];
+                String borough = row[6];
+                String latitude = row[7];
+                String longitude = row[8];
+                rat.setRatId(id);
+                rat.setDate(date);
+                rat.setLocType(locType);
+                rat.setZip(zip);
+                rat.setAddress(address);
+                rat.setCity(city);
+                rat.setBorough(borough);
+                rat.setLatitude(latitude);
+                rat.setLongitude(longitude);
+                ratList.add(rat);
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            mAdapter.notifyDataSetChanged();
+            spinner.setVisibility(View.GONE);
+        }
+    }
+
+    private void setupRecyclerView() {
+        mRecyclerView = findViewById(R.id.rat_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        //set layout
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        //add divider lines
+        DividerItemDecoration mDividerItemDecoration
+                = new DividerItemDecoration(mRecyclerView.getContext(),
+                LinearLayoutManager.VERTICAL);
+        mRecyclerView.addItemDecoration(mDividerItemDecoration);
+        //set adapter
+        ratList = new ArrayList<>();
+        mAdapter = new RatListAdapter(ratList, this);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
