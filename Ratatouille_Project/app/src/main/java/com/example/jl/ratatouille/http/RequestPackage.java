34feb5de75fcb74169dbbf948cffcca591da77cb
hallowpeno <chang.emily98@gmail.com>
2017-10-24 15:33:56 -0400

    added javadocs

--- Ratatouille_Project/app/src/main/java/com/example/jl/ratatouille/http/RequestPackage.java
+++ Ratatouille_Project/app/src/main/java/com/example/jl/ratatouille/http/RequestPackage.java
@@ -17,34 +17,68 @@ public class RequestPackage implements Parcelable {
     private String method = "GET";
     private Map<String, String> params = new HashMap<>();
 
+    /**
+     * Returns the end point of the request package
+     * @return end point
+     */
     public String getEndpoint() {
         return endPoint;
     }
 
+    /**
+     * Sets the request package's end point as the param value
+     * @param endPoint the new end point value
+     */
     public void setEndPoint(String endPoint) {
         this.endPoint = endPoint;
     }
 
+    /**
+     * gets the method variable
+     * @return the method
+     */
     public String getMethod() {
         return method;
     }
 
+    /**
+     * sets the method variable to the string parameter
+     * @param method the new mathod variable
+     */
     public void setMethod(String method) {
         this.method = method;
     }
 
+    /**
+     * gets the parameters for the request package
+     * @return the parameters for the request package
+     */
     public Map<String, String> getParams() {
         return params;
     }
 
+    /**
+     * sets the params for the request package
+     * @param params the new params
+     */
     public void setParams(Map<String, String> params) {
         this.params = params;
     }
 
+    /**
+     * sets the param's value to the new key and values
+     * @param key the key for the hash map
+     * @param value the value to enter
+     */
     public void setParam(String key, String value) {
         params.put(key, value);
     }
 
+    /**
+     * go through the params to find the encoded params value of
+     * UTF-8
+     * @return the string of the key and value of the encoded params
+     */
     public String getEncodedParams() {
         StringBuilder sb = new StringBuilder();
         for (String key : params.keySet()) {
@@ -79,9 +113,16 @@ public class RequestPackage implements Parcelable {
         }
     }
 
+    /**
+     * default constructor
+     */
     public RequestPackage() {
     }
 
+    /**
+     * reads in the the parcel to create anew request package
+     * @param in the parcel of data that creates the request package
+     */
     protected RequestPackage(Parcel in) {
         this.endPoint = in.readString();
         this.method = in.readString();
@@ -94,6 +135,9 @@ public class RequestPackage implements Parcelable {
         }
     }
 
+    /**
+     * Creates a new parcelable
+     */
     public static final Parcelable.Creator<RequestPackage> CREATOR = new Parcelable.Creator<RequestPackage>() {
         @Override
         public RequestPackage createFromParcel(Parcel source) {
