4709bf31be7a32417ea645bcbe538c44805fe329
j <jasmineliu@gatech.edu>
2017-10-26 19:52:38 -0400

    temporarily re-implement date edittext

--- Ratatouille_Project/app/src/main/java/com/example/jl/ratatouille/activity/AddActivity.java
+++ Ratatouille_Project/app/src/main/java/com/example/jl/ratatouille/activity/AddActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -25,8 +26,8 @@ import retrofit2.Callback;
 public class AddActivity extends AppCompatActivity {
 
     private EditText editDate, editLocType, editZip, editAddress, editCity, editBorough, editLatitude, editLongitude;
+    private DatePicker datePicker;
     private Button submitButton, cancelButton;
-    private ComponentName callingActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ Ratatouille_Project/app/src/main/java/com/example/jl/ratatouille/fragment/DatePickerFragment.java
@@ -0,0 +1,16 @@
+package com.example.jl.ratatouille.fragment;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+
+/**
+ * Created by jav on 10/25/2017.
+ */
+
+public class DatePickerFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+    }
+}
