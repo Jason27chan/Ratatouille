6deef72092fcd915c313b0e88480e379a8ea0f84
hallowpeno <chang.emily98@gmail.com>
2017-11-10 18:28:40 -0500

    Revert "help me"
    This reverts commit 4f34cc8bb0fff8c141d5941ec4020efda9d5b88d.

--- Ratatouille_Project/app/src/test/java/com/example/jl/ratatouille/UpdateDataTest.java
+++ Ratatouille_Project/app/src/test/java/com/example/jl/ratatouille/UpdateDataTest.java
@@ -19,6 +19,16 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.example.jl.ratatouille", appContext.getPackageName());
+    }
+}
+
 /**
  * Method JUnit Test, which tests if the RecyclerView is updating the RecyclerView
  */
