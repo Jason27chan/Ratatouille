4242a4a19bbf03ebda8244a8c91b6709687303ec
j <jasmineliu@gatech.edu>
2017-11-10 16:21:09 -0500

    maps test added

---
+++ Ratatouille_Project/app/src/test/java/com/example/jl/ratatouille/MapsActivityTest.java
@@ -0,0 +1,23 @@
+package com.example.jl.ratatouille;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by jav on 11/10/2017.
+ */
+
+public class MapsActivityTest {
+
+    /**
+     * This method is run before each test
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void test
+}
