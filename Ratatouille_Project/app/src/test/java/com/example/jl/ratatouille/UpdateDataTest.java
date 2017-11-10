cfdd84e5dd740625370e0dcf459da601dfdd6e3c
Unknown <shany198@gmail.com>
2017-11-10 17:08:42 -0500

    updateDataTest() still not finished
    WHY DOES THE ADD() NOT WORK

--- Ratatouille_Project/app/src/test/java/com/example/jl/ratatouille/UpdateDataTest.java
+++ Ratatouille_Project/app/src/test/java/com/example/jl/ratatouille/UpdateDataTest.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.example.jl.ratatouille.adapter.RecyclerViewAdapter;
 import com.example.jl.ratatouille.model.Rat;
 
 import org.junit.Test;
@@ -24,13 +25,19 @@ import static org.junit.Assert.*;
 public class UpdateDataTest {
     private List<Rat> myRatList = new ArrayList<>();
     Rat rat = new Rat();
+    myRatList.add(rat);
     // create many rats
 
     @Test
     public void useUpdateData() throws Exception {
+        private RecyclerViewAdapter newRecyclerView = new RecyclerViewAdapter();
+        private List<Rat> ratList = newRecyclerView.getRatList(); // does this ratList
+        newRecyclerView.updateData(); // params!
         // make an ArrayList<Rat> rats to be passed in
         // call updateData(List<Rat> rats) from RecyclerViewAdapter =
         // check ratList with ArrayList
-        // lots of assert statments
+        // lots of assert statements
+
+        assertEquals(newRecyclerView.getRatList()[0], myRatList[0]);
     }
 }
