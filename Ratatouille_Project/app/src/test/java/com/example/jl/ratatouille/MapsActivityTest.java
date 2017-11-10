43888884c404ae43987d01a173f056b0a8666f6e
j <jasmineliu@gatech.edu>
2017-11-10 16:42:12 -0500

    update maps junit

--- Ratatouille_Project/app/src/test/java/com/example/jl/ratatouille/MapsActivityTest.java
+++ Ratatouille_Project/app/src/test/java/com/example/jl/ratatouille/MapsActivityTest.java
@@ -1,7 +1,17 @@
 package com.example.jl.ratatouille;
 
+import com.example.jl.ratatouille.activity.MapsActivity;
+import com.example.jl.ratatouille.model.Rat;
+
+import junit.framework.Assert;
+
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
 
 /**
  * Created by jav on 11/10/2017.
@@ -9,15 +19,20 @@ import org.junit.Test;
 
 public class MapsActivityTest {
 
+    List<Rat> testRats;
+
     /**
      * This method is run before each test
      * @throws Exception
      */
     @Before
     public void setUp() throws Exception {
-
+        testRats = new ArrayList<>();
     }
 
     @Test
-    public void test
+    public void testUpdateMap() {
+        assertEquals(testRats.size(), MapsActivity.updateMap().size());
+
+    }
 }
