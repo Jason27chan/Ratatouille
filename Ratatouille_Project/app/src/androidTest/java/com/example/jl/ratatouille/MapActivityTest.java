0a47b5d200e36790c4fb67e2baf8831cb40dbe4a
Jason Chan <jason27chan@gmail.com>
2017-11-14 14:17:11 -0500

    sed reax only

--- Ratatouille_Project/app/src/androidTest/java/com/example/jl/ratatouille/MapActivityTest.java
+++ Ratatouille_Project/app/src/androidTest/java/com/example/jl/ratatouille/MapActivityTest.java
@@ -13,6 +13,7 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withHint;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -32,13 +33,46 @@ public class MapActivityTest {
     @Test
     public void testFilterButton() {
         onView(withId(R.id.map_filter)).perform(click());
-        onView(withId(R.id.filter_submit)).check(matches(withText("Submit")));
+        onView(withId(R.id.filter_submit)).check(matches(isDisplayed()));
     }
 
     @Test
     public void testAddRatButton() {
         onView(withId(R.id.btn_addRat_maps)).perform(click());
+        onView(withId(R.id.editTxt_created_date)).check(matches(isDisplayed()));
+        onView(withId(R.id.editTxt_incident_zip)).check(matches(isDisplayed()));
+        onView(withId(R.id.editTxt_incident_address)).check(matches(isDisplayed()));
+        onView(withId(R.id.editTxt_city)).check(matches(isDisplayed()));
+        onView(withId(R.id.editTxt_borough)).check(matches(isDisplayed()));
+        onView(withId(R.id.editTxt_latitude)).check(matches(isDisplayed()));
+        onView(withId(R.id.editTxt_longitude)).check(matches(isDisplayed()));
+        onView(withId(R.id.editTxt_borough)).check(matches(isDisplayed()));
+        onView(withId(R.id.btn_submit)).check(matches(isDisplayed()));
         onView(withId(R.id.btn_submit)).check(matches(withHint("Submit")));
+        onView(withId(R.id.btn_cancel)).check(matches(isDisplayed()));
+        onView(withId(R.id.btn_cancel)).check(matches(withHint("Cancel")));
+
+    }
+
+    @Test
+    public void testNavigateList() {
+        onView(withId(R.id.action_list)).perform(click());
+        onView(withId(R.id.rat_recycler_view)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testNavigateGraph() {
+        onView(withId(R.id.action_graph)).perform(click());
+        onView(withId(R.id.graph_day)).check(matches(isDisplayed()));
+        onView(withId(R.id.graph_month)).check(matches(isDisplayed()));
+        onView(withId(R.id.graph_year)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testNavigateSettings() {
+        onView(withId(R.id.action_settings)).perform(click());
+        onView(withId(R.id.btn_logout)).check(matches(isDisplayed()));
+        onView(withId(R.id.btn_logout)).check(matches(withText("Logout")));
     }
 
 }
